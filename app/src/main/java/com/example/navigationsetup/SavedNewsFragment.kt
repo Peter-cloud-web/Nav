b95aadc7bf340eb1413e20478b682df2298a5bce
Peter-cloud-web <petergichia165@gmail.com>
2021-11-14 22:35:05 +0300

    Add fragments and add them to the nav graph

---
+++ app/src/main/java/com/example/navigationsetup/SavedNewsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.navigationsetup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
