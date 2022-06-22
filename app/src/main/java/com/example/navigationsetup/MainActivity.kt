b6e1f1e5a75b1bd504aafdac9092992f8fd7804a
Peter-cloud-web <petergichia165@gmail.com>
2022-06-22 11:00:06 +0300

    Fix navigation setup

--- app/src/main/java/com/example/navigationsetup/MainActivity.kt
+++ app/src/main/java/com/example/navigationsetup/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.navigationsetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigationsetup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setUpNavigation(binding)
+    }
+
+    private fun setUpNavigation(binding:ActivityMainBinding){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
 
     }
 }
